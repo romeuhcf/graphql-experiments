11e0e520538c46d101a9bd71ef9569aea18bc4c0
Romeu Fonseca <romeu.hcf@gmail.com>
2020-02-11 16:54:18 -0300

    fix n+1 with batch loader

--- app/graphql/types/message_type.rb
+++ app/graphql/types/message_type.rb
@@ -6,8 +6,11 @@ module Types
     field :destination, String, null: true
 
     def origin
-      User.find(object.from_user_id).name
+      BatchLoader::GraphQL.for(object.from_user_id).batch do |user_ids, loader|
+        User.where(id: user_ids).select(:id, :name).each { |user| loader.call(user.id, user.name) }    
+      end
     end
+    
     def destination
       User.find(object.to_user_id).name
     end
